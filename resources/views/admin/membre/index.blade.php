576292ba5d9f2f27b0f49109d1eddb985d6a5615
VVood-09 <dubois9663@gmail.com>
2023-03-20 12:31:46 -0400

    admin.membre.index complet sans paginate. CSS complet

--- resources/views/admin/membre/index.blade.php
+++ resources/views/admin/membre/index.blade.php
@@ -15,8 +15,8 @@
             <td>{{$membre->name}}</td>
             <td>{{$membre->email}}</td>
             <td>{{$membre->permissionUtilisateur->permission}}</td>
-            <td><a href="#">Modifier</a></td>
-            <td><a href="#">Supprimer</a></td>
+            <td><a href="#" class="modifier">Modifier</a></td>
+            <td><a href="#" class="supprimer">Supprimer</a></td>
         </tr>
         @empty
         <h2>Aucun membres dans la base de donnée.</h2>
