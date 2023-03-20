a7cfec9094c2dc17b92f31f94fc0553ea22c1671
VVood-09 <dubois9663@gmail.com>
2023-03-20 14:38:38 -0400

    View admin\membre\show presque complet, faire la validation en JS

--- app/Http/Controllers/Admin/AdminUtilisateurController.php
+++ app/Http/Controllers/Admin/AdminUtilisateurController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Permission;
 use App\Models\User;
+use Illuminate\Validation\Rule;
 
 class AdminUtilisateurController extends Controller
 {
@@ -21,7 +22,24 @@ class AdminUtilisateurController extends Controller
         return view('admin.membre.show', ['utilisateur' => $utilisateur, 'permissions' => $permissions]);
     }
 
-    public function update(Request $request){
-        return $request;
+    public function update(Request $request, User $utilisateur){
+        
+        $request->validate([
+            'name' => 'required',
+            // Pour valider le courriel unique de l'utilisateur à modifier
+            // https://laravel.com/docs/master/validation#rule-unique
+            'email'=> ['required', 'email', Rule::unique('users')->ignore($utilisateur->id)],
+            'permission_id' => 'required|exists:permissions,id'
+        ]);
+
+        $utilisateur->update([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'permission_id'=>$request->permission_id
+        ]);
+
+        $utilisateurs = User::all();
+      
+        return view('admin.membre.index', ['utilisateurs'=>$utilisateurs]);
     }
 }
--- resources/views/admin/membre/show.blade.php
+++ resources/views/admin/membre/show.blade.php
@@ -2,6 +2,26 @@
 @section('title', 'Admin')
 @section('content')
 
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+<div>
+    @if($errors)
+      <ul>
+        @foreach($errors->all() as $error)
+        <li class="text-danger">{{ $error }}</li>
+        @endforeach
+      </ul>
+    @endif
+</div>
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+<!-- CHANGEZ LA GESTION DES MESSAGES D'ERREURS -->
+
     <form action="" method="post" class="formBtl_form">
         @csrf
         @method('put')
@@ -15,12 +35,16 @@
         <label for="permission">Niveau d'accès</label>
         <select id="permission" name="permission_id" >
             @forelse($permissions as $permission)
-                <option value="{{$permission->id}}">{{$permission->permission}}</option>
+                <option value="{{$permission->id}}" 
+                    @if($permission->id == $utilisateur->permission_id)
+                    selected
+                    @endif>
+                    {{$permission->permission}}</option>
             @empty
             @endforelse
         </select>
 
-        <input type="submit" value="Mettre à jour">
+        <input class="btn" type="submit" value="Mettre à jour">
         
     </form>
 
