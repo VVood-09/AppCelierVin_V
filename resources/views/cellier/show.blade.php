b919a620457d3c8aba03898f636dd35d7b4a3f75
VVood-09 <dubois9663@gmail.com>
2023-03-27 10:22:04 -0400

    Denormalisation, retrait de loop in loop pour cellier/show, pour optimiser

--- resources/views/cellier/show.blade.php
+++ resources/views/cellier/show.blade.php
@@ -131,7 +131,7 @@
                 <div>Aucune bouteille dans le cellier</div>
             </template>
     
-            <template x-for="item in items">
+            <template x-for="(item, cle) in items">
                 <article class="liste-btl_carte">
                     <div class="liste-btl_tete">
                         <div class="liste-btl_tete_pays">
@@ -171,19 +171,27 @@
                             </div>
 
                             {{-- Avec l'utilisation de Alpine.js pour Sort() et de Laravel, la syntaxe a employé limite la manière de faire les choses. Dans le cas que la composante Quantite doit être usé avec un object contenant les informations de `item` provenant d'alpine `x-data`, `item` n'est pas un objet transferable dans la composante donc il doit être créé manuellement avec les informations de $bouteilles. `item` ne peut pas non plus être mis dans une variable pour php puisque c'est du JSON front-end. --}}
-                            @forelse($bouteilles as $bouteille)
-                            <template x-if="item.nom.toString() == '{{$bouteille->nom}}'">
+                            {{-- Cette boucle est trop exigeante sur des grosses liste --}}
+                            {{-- @forelse($bouteilles as $bouteille)
+                            <template x-if="item.nom.toString() == `{{$bouteille->nom}}`">
                                 @php
                                 $item = (object)array('qte' => $bouteille->qte, 'id' => $bouteille->id);
                                 @endphp
                                 <x-quantite :bouteille="$item" :cellier="$cellier"/>
                             </template>
                             @empty
-                            @endforelse
+                            @endforelse --}}
                             {{-- Malheureusement, 
                                 https://gist.github.com/jasonlbeggs/1341e7367c0dc69ac64ef2140d0f0591
                                 ne semble pas fonctionner. --}}
                             {{-- <x-quantite ::bouteille="item" :cellier="$cellier"/> --}}
+                            <form  class="liste-btl_info_bouteilles" x-data="{counter : item.qte, idB : item.id, idC : {{$cellier->id}} }" x-on:change="changeQte" title="Change quantité de bouteille" onsubmit="return false">
+                                <button @click="counter--; if(counter <0){counter = 0}" x-on:click="changeQte" aria-label="Enlever quantité"><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g data-name="Layer 2"><path d="M16 29a13 13 0 1 1 13-13 13 13 0 0 1-13 13Zm0-24a11 11 0 1 0 11 11A11 11 0 0 0 16 5Z" fill="#7e001e" class="fill-000000"></path><path d="M22 17H10a1 1 0 0 1 0-2h12a1 1 0 0 1 0 2Z" fill="#7e001e" class="fill-000000"></path></g><path d="M0 0h32v32H0z" fill="none"></path></svg></button>
+                            
+                                <input type="number" x-on:change="changeQte" x-model.number="counter" min="0" aria-label="Quantité">
+                                                   
+                                <button @click="counter++" x-on:click="changeQte" aria-label="Ajouter quantité"><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g data-name="Layer 2"><path d="M16 29a13 13 0 1 1 13-13 13 13 0 0 1-13 13Zm0-24a11 11 0 1 0 11 11A11 11 0 0 0 16 5Z" fill="#7e001e" class="fill-000000"></path><path d="M16 23a1 1 0 0 1-1-1V10a1 1 0 0 1 2 0v12a1 1 0 0 1-1 1Z" fill="#7e001e" class="fill-000000"></path><path d="M22 17H10a1 1 0 0 1 0-2h12a1 1 0 0 1 0 2Z" fill="#7e001e" class="fill-000000"></path></g><path d="M0 0h32v32H0z" fill="none"></path></svg></button>
+                            </form>
                         </div>
                     </div>
                 </article>
