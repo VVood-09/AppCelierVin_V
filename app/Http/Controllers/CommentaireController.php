2aa6bda853cd68fb5441d1b7ff74a4a6a487eb63
Pénélope Ducharme <88218376+Pe-nelo-pe@users.noreply.github.com>
2023-04-05 16:31:58 -0400

    commentaires sql

--- app/Http/Controllers/CommentaireController.php
+++ app/Http/Controllers/CommentaireController.php
@@ -16,10 +16,11 @@ use Illuminate\Support\Facades\Auth;
 
 class CommentaireController extends Controller{
 
-    // public function create(Request $request){}
-
-
-
+    /**
+    *Stocke un nouveau commentaire dans la base de données.
+    *@param \Illuminate\Http\Request $request
+    *@return \Illuminate\Http\JsonResponse
+    */
     public function store(Request $request){
         $comment = Commentaire::create([
             'commentaire'=>$request->commentaire,
@@ -27,27 +28,8 @@ class CommentaireController extends Controller{
             'bouteille_id'=> $request->bouteille_id
         ]);
         
-      return response()->json(['data' => $comment,'success' => true, 'message' => 'Commentaire ajouté']);
+        return response()->json(['data' => $comment,'success' => true, 'message' => 'Commentaire ajouté']);
    
-    }
-
-
-    // public function show(Cellier $cellier, Bouteille $bouteille ){}
-    
-    
-    public function edit(  Cellier $cellier,Bouteille $bouteille){
-   
-    }
-
-
-    public function update(Request $request, Bouteille $bouteille, Cellier $cellier){
-
-    }
-
-
-
-    public function destroy(Cellier $cellier,Bouteille $bouteille,){
-        
     }
 
 
